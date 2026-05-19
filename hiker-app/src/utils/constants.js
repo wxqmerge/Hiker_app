3a29761f6a73e50d23252550c95b05895a3fcf17
wxqmerge <wxqmerge@outlook.com>
2026-05-19 08:18:03 -0700

    audits

---
+++ hiker-app/src/utils/constants.js
@@ -0,0 +1,22 @@
+// Shared constants
+
+export const MONTH_NAMES = [
+  'January', 'February', 'March', 'April', 'May', 'June',
+  'July', 'August', 'September', 'October', 'November', 'December'
+];
+
+export const MONTH_ABBR = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+export const DAY_NAMES = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+export const DEFAULT_FILTERS = {
+  search: '',
+  distanceMin: 0,
+  distanceMax: 20,
+  elevationMin: 0,
+  elevationMax: 5000,
+  difficulties: [],
+  months: [],
+  sortBy: 'name',
+  wilderness: false
+};
---
+++ hiker-app/src/utils/formatTrail.js
@@ -0,0 +1,21 @@
+import { getRideCost } from './report';
+
+// Format trail core info for text output
+export function formatTrailLine(trail) {
+  let name = trail.fullName || trail.name;
+  name = name.replace(/◆\uFE0E?$/, '').replace(/◆+$/, '');
+
+  const difficulty = `[${trail.difficulty}]`;
+  let distanceText = trail.distance != null ? trail.distance.toFixed(1) : 'N/A';
+  if (trail.distanceExtended) distanceText += `-${trail.distanceExtended.toFixed(1)}`;
+  const elevStart = trail.elevationStart != null ? trail.elevationStart.toLocaleString() : '0';
+  const elevMax = trail.elevationMax != null ? trail.elevationMax.toLocaleString() : elevStart;
+  const elevationText = `${elevStart}'-${elevMax}'`;
+  const parking = trail.parking || '';
+  const rideCost = trail.range ? getRideCost(parseInt(trail.range)) : '';
+
+  let line = `${name}◆︎  ${difficulty}\t${distanceText} / ${elevationText}\t${parking}`;
+  if (rideCost) line += `\t${rideCost}`;
+
+  return line;
+}
